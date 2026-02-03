107c76dd94db44d156b5b9d884b124d10dda8cc8
knowingfly <knowingfly@gmail.com>
2026-02-03 17:09:58 +0700

    chore: create chat page file structure

---
+++ app/chat/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import {useState} from 'react';
+
+export default function ChatPage() {
+    return (
+        <div className="flex h-screen bg-gray-50">
+            <h1>Chat Page</h1>
+        </div>
+    );
+}
